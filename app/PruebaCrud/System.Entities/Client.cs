445f5067a656a4438202c8879792e559b52ae989
René Meléndez <35284243+ReneMel@users.noreply.github.com>
2021-11-05 15:28:04 -0600

    [add] Objet Client

--- app/PruebaCrud/System.Entities/Client.cs
+++ app/PruebaCrud/System.Entities/Client.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace System.Entities
 {
-    class Client
+    public class Client
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string DUI { get; set; }
+        public DateTime Birthday { get; set; }
+
+        
     }
 }
